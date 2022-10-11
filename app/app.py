27f29958174aa61f7cf369135b0d51fb1a0cf682
Tishh95 <quentinurschfeld@gmail.com>
2022-10-11 10:29:00 -0400

    add insert and test

--- app/app.py
+++ app/app.py
@@ -1,8 +1,23 @@
-from flask import Flask, jsonify, render_template, request
-import pymongo
+from flask import Flask, render_template, request
+from flask_pymongo import PyMongo
 from pymongo import MongoClient
 
 app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'quizdb'
+app.config['MONGO_AUTH_SOURCE'] = 'admin'
+app.config['MONGO_URI'] = 'mongodb://root:root@<url>:27017/admin'
+
+mongo = PyMongo(app)
+@app.route('/')
+def admin():
+    return render_template('index.html')
+
+@app.route('/listq', methods = ['GET'])
+def get_animals():
+    db = get_db()
+    _questions = db.question.find({})
+    return render_template('index.html', results = _questions)
 
 
 def get_db():
@@ -10,26 +25,18 @@ def get_db():
                          port=27017,
                          username='root',
                          password='root',
-                        authSource="admin")
+                         authSource="admin")
     db = client["quizdb"]
     return db
 
-@app.route('/')
-def ping_server():
-    return "Welcome to the world of animals."
 
-@app.route('/index', methods = ['GET'])
-def get_animals():
+@app.route('/mongo', methods=['GET'])
+def get_all_docs():
     db = get_db()
-    _questions = db.question.find({})
-    return render_template('index.html',results = _questions)
-
-@app.route('/admin', methods = ['GET','POST'])
-def admin():
-    return render_template('admin.html')
-
-@app.route('/database', methods = ['POST'])
-def database():
+    doc = db.question.insert_one({'abcd':'abcd'})
+    return "Inserted"
+@app.route('/insert', methods=['GET'])
+def insertData():
     db = get_db()
     url = request.form['question']
     answer = request.form['answer']
@@ -37,6 +44,5 @@ def database():
     db.question.insert_one({"url":url,"answer":answer})
     return render_template('index.html', results = dict)
 
-
-if __name__=='__main__':
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
--- app/tests/test.py
+++ app/tests/test.py
@@ -1,7 +1,9 @@
-import pytest
-from pymongo import MongoClient
+import unittest
 
+from app import app
 
-@pytest.fixture
-def client():
-    return app.test_client()
+class BasicTestCase(unittest.TestCase):
+    def test_home(self):
+        tester = app.test_client(self)
+        response = tester.get('/', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
